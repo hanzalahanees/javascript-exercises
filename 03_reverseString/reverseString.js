70873dee537b84c287f254fec2e1bf88f7d65c72
Fazal Ur Rehman <anees_pk4@yahoo.com>
2023-01-31 09:26:20 +0500

    passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+    /* var splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray; */
 
 };
 
