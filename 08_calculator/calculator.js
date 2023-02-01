92c338d1a9def94ee90fec3143e189236c84896a
Fazal Ur Rehman <anees_pk4@yahoo.com>
2023-02-01 22:26:50 +0500

    passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,35 @@
-const add = function() {
+const add = function(s1,s2) {
+  return s1+s2;
 	
 };
 
-const subtract = function() {
+const subtract = function(a1,a2) {
+  if(a1>a2){
+    return a1-a2;
+  }
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 	
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, nextItem) => accumulator * nextItem);
 };
 
-const power = function() {
+const power = function(z1,z2) {
+  return Math.pow(z1,z2)
 	
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 	
 };
 
